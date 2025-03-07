3cb99350fa83524a5e216ef89b2d0c1f7ebbdff6
sudhansingh654 <sudhansingh654@gmail.com>
2025-03-07 12:02:58 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    string name = "Alice";
+    int age = 30;
+    cout << "My name is " << name << ", and I am " << age << " years old." << endl;
+    return 0;
+}
