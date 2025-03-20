ee32bc49db007b58f2bddde962c0a61f5510cd88
sudhansingh654 <sudhansingh654@gmail.com>
2025-03-20 17:56:15 +0000

    Add 14.cpp

---
+++ src/14.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    if (a > 10) {
+        cout << "The value of a is greater than 10." << endl;
+    } else {
+        cout << "The value of a is not greater than 10." << endl;
+    }
+    return 0;
+}
