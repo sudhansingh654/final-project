8122720f3078d978e4052d5eaecce62fd97d75b2
sudhansingh654 <sudhansingh654@gmail.com>
2025-04-08 08:47:25 +0000

    Add 26.cpp

---
+++ src/26.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    cout << "The value of a is: " << a << endl;
+
+    if (a > 10) {
+        cout << "a is greater than 10." << endl;
+    } else {
+        cout << "a is not greater than 10." << endl;
+    }
+
+    return 0;
+}
