c3e82a3dbad796f0f07f4ad9dbe33322ecdbbe97
sudhansingh654 <sudhansingh654@gmail.com>
2025-04-27 01:46:13 +0000

    Add 32.cpp

---
+++ src/32.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+    
+    if (x > y) {
+        cout << "X is greater than Y" << endl;
+    } else {
+        cout << "Y is greater than X" << endl;
+    }
+    
+    return 0;
+}
