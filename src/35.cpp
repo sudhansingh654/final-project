eb18ac0e88c3b296583ff0baa39f23972dd0c4a4
sudhansingh654 <sudhansingh654@gmail.com>
2025-05-19 19:03:06 +0000

    Add 35.cpp

---
+++ src/35.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    
+    if (a > b) {
+        cout << "A is greater than B." << endl;
+    } else {
+        cout << "B is greater than A." << endl;
+    }
+    
+    return 0;
+}
